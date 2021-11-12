6d86cfa7b51d504fa5f413a3744c94bf3b4fba93
likhith00 <likhith.prudhivi@gmail.com>
2021-11-12 15:51:54 +0530

    stage 1 complete

---
+++ src/load_data.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import yaml
+import argparse
+from get_data import get_data,read_params
+
+
+def load_and_save(config_path):
+    config  = read_params(config_path)
+    df = get_data(config_path)
+    new_cols = [col.replace(" ","_") for col in df.columns]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    df.to_csv(raw_data_path,sep=",",header=new_cols,index=False)
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args = args.parse_args()
+    load_and_save(parsed_args.config)
