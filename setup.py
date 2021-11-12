f8f5c418d79c3f833e66037248c578d441d01c0c
likhith00 <likhith.prudhivi@gmail.com>
2021-11-12 21:06:05 +0530

    setup.py created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "predict_carprices",
+    version ="0.0.1",description="its an mlops implementation of car prices dataset",
+    author = "Likhith",
+    packages= find_packages(),
+    license= "MIT"
+)
